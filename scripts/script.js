f12b0aa3c9b5b72653266524aef079f2430e785f
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-02-24 09:51:50 -0600

    Add contriutions message and some improvements (#18)
    * Added ico icon directly in project
    
    * Reduded unused vertical spaces
    
    * Improved timer label count to match better what is happening
    
    * Increased volume of alarm sound
    
    * Added contribution buttons
    
    * Added MadeBy message
    
    * Added analytics

--- scripts/script.js
+++ scripts/script.js
@@ -24,30 +24,8 @@ function createTimers()
     });
 }
 
-function createTimers()
-{
-    const container = document.getElementById('timers-container');
-
-    bosses.forEach(boss => {
-        let div = document.createElement('div');
-        div.classList.add('timer');
-        div.innerHTML = `
-            <button id="${boss.id}-button" onclick="startTimer(${boss.minutes}, ${boss.seconds}, '${boss.id}', '${boss.displayName}')">
-                <a href="${boss.guideUrl}" target="_blank" class="external-link" title="Check guide" onclick="handleExternalLinkClick(event)">
-                    <i class="fas fa-book"></i>
-                </a>
-                <img src="${boss.gifUrl}" alt="${boss.displayName}">
-                <div class="timer-info">
-                    <h3>${boss.displayName}</h3>
-                    <h1 id="${boss.id}">${formatTime((boss.minutes * 60) + boss.seconds)}</h1>
-                </div>
-            </button>
-        `;
-        container.appendChild(div);
-    });
-}
-
 function startTimer(minutes, seconds, timerId, displayName) {
+    Analytic_TimerStarted(timerId);
     stopCurrentTimer();
 
     let timerData = {
@@ -59,24 +37,39 @@ function startTimer(minutes, seconds, timerId, displayName) {
     };
 
     document.getElementById(`${timerData.timerId}-button`).classList.add('selected');
+    document.getElementById(timerData.timerId).innerText = formatTime(Math.floor(timerData.totalSeconds - 1));
 
     let start = Date.now();
     timerData.intervalId = setInterval(() => {
         currentTimer.secondsElapsed = (Date.now() - start) / secondInMilliseconds;
+        let remainingSeconds = timerData.totalSeconds - currentTimer.secondsElapsed;
 
         document.getElementById(timerData.timerId).innerText = formatTime(Math.floor(timerData.totalSeconds - timerData.secondsElapsed));
         refreshTitle();
-    
-        if (currentTimer.secondsElapsed > timerData.totalSeconds) {
-            stopCurrentTimer();
-            document.getElementById('alarm').play();
 
+        // Sound alarm when the system shows 00:00
+        if (remainingSeconds <= 1 && remainingSeconds > 0) {
+            document.getElementById('alarm').play();
+        }
+    
+        // Reset when it reaches real 0
+        if (remainingSeconds <= 0) {
+            stopCurrentTimer();
+            Analytic_TimerRestarted(timerId);
             startTimer(minutes, seconds, timerId, displayName);
         }
     }, secondInMilliseconds);
 
     currentTimer = timerData;
     refreshTitle();
+}
+
+function forceStopTimer() {
+    if (currentTimer) {
+        Analytic_ForcedStopTimer(currentTimer.timerId);
+    }
+
+    stopCurrentTimer();
 }
 
 function stopCurrentTimer() {
@@ -108,4 +101,37 @@ function refreshTitle()
 
 function handleExternalLinkClick(event) {
     event.stopPropagation();
+}
+
+//// ANALYTICS ////
+
+function Analytic_TimerStarted(timerId)
+{
+    if (currentTimer && currentTimer.timerId === timerId){
+        gtag('event', 'timer', {
+            'event_category': 'Resetted Timer',
+            'event_label': timerId
+        });
+    }
+
+    gtag('event', 'timer', {
+        'event_category': 'Started',
+        'event_label': timerId
+    });
+}
+
+function Analytic_TimerRestarted(timerId)
+{
+    gtag('event', 'timer', {
+        'event_category': 'Restarted',
+        'event_label': timerId
+    });
+}
+
+function Analytic_ForcedStopTimer(timerId)
+{
+    gtag('event', 'timer', {
+        'event_category': 'Forced Stop',
+        'event_label': timerId
+    });
 }
