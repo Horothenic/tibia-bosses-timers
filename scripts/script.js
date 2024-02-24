473365d30c1cf4fc2f5723637bd677eb8954d4e2
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-02-24 10:49:09 -0600

    Bug fixing favicon and analytics (#19)
    * Fixed favicon position
    
    * Changed rel of favicon
    
    * Trying fixing analytics

--- scripts/script.js
+++ scripts/script.js
@@ -102,36 +102,3 @@ function refreshTitle()
 function handleExternalLinkClick(event) {
     event.stopPropagation();
 }
-
-//// ANALYTICS ////
-
-function Analytic_TimerStarted(timerId)
-{
-    if (currentTimer && currentTimer.timerId === timerId){
-        gtag('event', 'timer', {
-            'event_category': 'Resetted Timer',
-            'event_label': timerId
-        });
-    }
-
-    gtag('event', 'timer', {
-        'event_category': 'Started',
-        'event_label': timerId
-    });
-}
-
-function Analytic_TimerRestarted(timerId)
-{
-    gtag('event', 'timer', {
-        'event_category': 'Restarted',
-        'event_label': timerId
-    });
-}
-
-function Analytic_ForcedStopTimer(timerId)
-{
-    gtag('event', 'timer', {
-        'event_category': 'Forced Stop',
-        'event_label': timerId
-    });
-}
