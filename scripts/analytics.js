c4283ad73fc9ee21109d9329ea27c09d12a7fe2a
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-02-25 12:28:45 -0600

    Changed Google Analytics with amplitude

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,30 +1,69 @@
 function Analytic_TimerStarted(timerId)
 {
+    let amplitudeEvents = [];
     if (currentTimer && currentTimer.timerId === timerId){
-        gtag('event', 'timer', {
-            'event_category': 'Resetted Timer',
-            'event_label': timerId
+        amplitudeEvents.push({
+            "user_id": "Generic",
+            "event_type": "Timer Resetted",
+            "event_properties": {
+                "timer_id": timerId
+            }
         });
     }
 
-    gtag('event', 'timer', {
-        'event_category': 'Started',
-        'event_label': timerId
+    amplitudeEvents.push({
+        "user_id": "Generic",
+        "event_type": "Timer Started",
+        "event_properties": {
+            "timer_id": timerId
+        }
     });
+
+    sendEventsToAmplitude(amplitudeEvents);
 }
 
 function Analytic_TimerRestarted(timerId)
 {
-    gtag('event', 'timer', {
-        'event_category': 'Restarted',
-        'event_label': timerId
+    let amplitudeEvents = [];
+    amplitudeEvents.push({
+        "user_id": "Generic",
+        "event_type": "Timer Restarted",
+        "event_properties": {
+            "timer_id": timerId
+        }
     });
+
+    sendEventsToAmplitude(amplitudeEvents);
 }
 
 function Analytic_ForcedStopTimer(timerId)
 {
-    gtag('event', 'timer', {
-        'event_category': 'Forced Stop',
-        'event_label': timerId
+    let amplitudeEvents = [];
+    amplitudeEvents.push({
+        "user_id": "Generic",
+        "event_type": "Timer Forced Stop",
+        "event_properties": {
+            "timer_id": timerId
+        }
+    });
+
+    sendEventsToAmplitude(amplitudeEvents);
+}
+
+function sendEventsToAmplitude(events) {
+    $.ajax({
+        url: 'https://api2.amplitude.com/2/httpapi',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': '*/*'
+        },
+        data: JSON.stringify({
+            "api_key": "939db8a9d6769eab5bdf71544be582c2",
+            "events": events
+        }),
+        success: function () {
+            console.log("Events sent to Amplitude:", events);
+        }
     });
 }
