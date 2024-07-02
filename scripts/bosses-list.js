54c66056f14326b60511e775b9bf230ceff71bd5
Ivan Herrera <ivan.herrera.m@outlook.com>
2024-07-01 18:17:08 -0600

    Added full moon bosses

--- scripts/bosses-list.js
+++ scripts/bosses-list.js
@@ -6,7 +6,8 @@ const baneBosses = [
         displayName: 'Heoni',
         guideUrl: 'https://www.youtube.com/watch?v=H09LVfDpZl8',
         gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1708572875/tibia-bane-bosses-farming-timers/heoni.webp',
-        tooltip: ''
+        tooltip: '',
+        group: ""
     },
     { 
         id: 'black-knight',
@@ -15,7 +16,8 @@ const baneBosses = [
         displayName: 'Black Knight',
         guideUrl: 'https://www.youtube.com/watch?v=5FAeMzksGqc',
         gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1708572875/tibia-bane-bosses-farming-timers/black-knight.webp',
-        tooltip: ''
+        tooltip: '',
+        group: ""
     },
     { 
         id: 'diseased',
@@ -24,7 +26,8 @@ const baneBosses = [
         displayName: 'Diseased x3',
         guideUrl: 'https://www.youtube.com/watch?v=NGFseS_XI7I,',
         gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1708877549/tibia-bane-bosses-farming-timers/diseased-bros.gif',
-        tooltip: ''
+        tooltip: '',
+        group: ""
     },
     { 
         id: 'glitterscale',
@@ -33,8 +36,42 @@ const baneBosses = [
         displayName: 'Glitterscale',
         guideUrl: 'https://www.youtube.com/watch?v=1DT9wr9wiYE',
         gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1709265621/tibia-bane-bosses-farming-timers/glitterscale.webp',
-        tooltip: 'There is a tile where you can stay and the boss will spawn and walk towards you so you can avoid using the timer.'
+        tooltip: 'There is a tile where you can stay and the boss will spawn and walk towards you so you can avoid using the timer.',
+        group: ""
     }
+];
+
+const archfoeBosses = [
+    { 
+        id: 'tamru-the-black',
+        minutes: 30,
+        seconds: 0,
+        displayName: 'Tamru',
+        guideUrl: 'https://www.youtube.com/watch?v=s77JxerP1KQ',
+        gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1719878588/tibia-bane-bosses-farming-timers/tamru_the_black.gif',
+        tooltip: '',
+        group: "full-moon"
+    },
+    { 
+        id: 'ayana-the-crimson-curse',
+        minutes: 30,
+        seconds: 0,
+        displayName: 'Ayana',
+        guideUrl: 'https://www.youtube.com/watch?v=s77JxerP1KQ',
+        gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1719878588/tibia-bane-bosses-farming-timers/ayana_the_crimson_curse.gif',
+        tooltip: '',
+        group: "full-moon"
+    },
+    { 
+        id: 'owin',
+        minutes: 30,
+        seconds: 0,
+        displayName: 'Owin',
+        guideUrl: 'https://www.youtube.com/watch?v=1kP6PObh4GY',
+        gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1719878589/tibia-bane-bosses-farming-timers/owin.gif',
+        tooltip: '',
+        group: "full-moon"
+    },
 ];
 
 const eventBosses = [
@@ -45,6 +82,7 @@ const eventBosses = [
         displayName: 'Bane Lord',
         guideUrl: 'https://www.youtube.com/watch?v=Pip3dN_StkU',
         gifUrl: 'https://res.cloudinary.com/drojh09io/image/upload/v1719877762/tibia-bane-bosses-farming-timers/bane_lord.gif',
-        tooltip: 'The possibility of an event is exact so try to start the timer a few seconds before the cue.'
+        tooltip: 'The possibility of an event is exact so try to start the timer a few seconds before the cue.',
+        group: ""
     },
 ];
